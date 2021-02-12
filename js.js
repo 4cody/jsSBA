e41852fdce98d6fa7522f482836771c1c0ce2b91
cody <cshaner04@gmail.com>
2021-02-12 15:43:24 -0600

    complete for turn in

--- js.js
+++ js.js
@@ -1,7 +1,32 @@
-// gsap.to("#logo", {duration: 5, x: 500});
+const sub = document.querySelector('#survey_submit')
+const radioInputs = document.getElementsByName('own');
+const favInput = document.getElementById('favorite')
+const useInputs = document.querySelectorAll('.check_input')
 
-const appearOptions = {};
+let answerSet = {
+    Owns: '',
+    Favorite: '',
+    Uses: []
+}
 
-const  fadeInOnScroll = new IntersectionObserver((entriesm app)=> {
 
+sub.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    for(let i = 0; i < radioInputs.length; i++) {
+        if (radioInputs[i].checked) {
+            answerSet.Owns = radioInputs[i].value
+            break;
+        }
+    }
+
+    answerSet.Favorite = favInput.value
+
+    for(let i = 0; i< useInputs.length; i++) {
+        if(useInputs[i].checked) {
+            answerSet.Uses.push(useInputs[i].value)
+        }
+    }
+
+    console.log(answerSet)
 })
